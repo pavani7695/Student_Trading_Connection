7dca1900542adfdce32c1a69d7a0d00f5c9d22ed
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-10-22 17:55:06 -0500

    added http put request to update the product details

--- backend/src/main/java/com/app/backend/Controller/ProductController.java
+++ backend/src/main/java/com/app/backend/Controller/ProductController.java
@@ -2,7 +2,9 @@ package com.app.backend.Controller;
 
 import com.app.backend.Entity.Product;
 import com.app.backend.Entity.User;
+import com.app.backend.Repository.ProductRepository;
 import com.app.backend.Service.ProductService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,30 +22,59 @@ public class ProductController {
         this.productService = productService;
     }
 
+    // Add Product
     @PostMapping("/saveProduct")
     public Product saveProduct(@RequestBody Product product) {
-        System.out.println("Title:"+product.getTitle());
         return productService.saveProduct(product);
     }
 
+    // Ger all the Products
     @GetMapping("")
     public List<Product> getProducts() {
         return productService.getProducts();
     }
 
+    // Get Product by productID
     @GetMapping("{productId}")
     public Product getProductByID(@PathVariable int productId) {
         return productService.getProductByID(productId);
     }
 
+    // Get List of Products by sellerID
     @GetMapping("/seller/{sellerID}")
     public List<Product> getProductBySellerID(@PathVariable int sellerID){
         return productService.getProductsBySellerID(sellerID);
     }
 
+    // Delete Product
     @DeleteMapping("{productID}")
     public void deleteProductByProductID(@PathVariable int productID){
         productService.deleteProductByID(productID);
+    }
+
+    // Update or Edit Product Details
+//    @PutMapping("{productID}")
+//    public Product updateProduct(@PathVariable int productID, @RequestBody Product product) {
+//        Product updateProduct = productService.getProductByID(productID);
+//        updateProduct.setCategory(product.getCategory());
+//        updateProduct.setPrice(product.getPrice());
+//        updateProduct.setStatus(product.getStatus());
+//        updateProduct.setDescription(product.getDescription());
+//        updateProduct.setBuyerID(product.getBuyerID());
+//        updateProduct.setTitle(product.getTitle());
+//
+//        productService.saveProduct(updateProduct);
+//
+//        return product;
+//    }
+
+    @PutMapping("{productID}")
+    public Product updateProduct(@PathVariable int productID, @RequestBody Product product) {
+        Product updateProduct = productService.getProductByID(productID);
+        BeanUtils.copyProperties(product, updateProduct, "id"); // Exclude copying 'id' property
+        productService.saveProduct(updateProduct);
+
+        return updateProduct;
     }
 
 }
